6bd166912d2a932b028bd7c1ead8927d7bb104cd
hadhee17 <hadhee58@gmail.com>
2025-09-29 00:13:19 +0530

    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login";
-import Register from "./pages/Register";
+import Register from "./pages/register";
 import Customer from "./pages/Customer";
 import { useAuth } from "./context/AuthContext";
 
