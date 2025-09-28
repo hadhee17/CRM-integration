2d03704bc19f4f7a17bb2a740a7c6216c69182fb
hadhee17 <hadhee58@gmail.com>
2025-09-28 23:43:30 +0530

    deployment updates

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,19 +1,25 @@
 import axios from "axios";
 
 export const login = async ({ username, password }) => {
-  const res = await axios.post("http://localhost:3000/api/login", {
-    username,
-    password,
-  });
+  const res = await axios.post(
+    "https://crm-integration-ruddy.vercel.app/api/login",
+    {
+      username,
+      password,
+    }
+  );
 
   // ✅ Return full response: { token, user }
   return res.data;
 };
 
 export const register = async ({ username, password }) => {
-  const res = await axios.post(`http://localhost:3000/api/register`, {
-    username,
-    password,
-  });
+  const res = await axios.post(
+    `https://crm-integration-ruddy.vercel.app/api/register`,
+    {
+      username,
+      password,
+    }
+  );
   return res.data.user; // { token, user }
 };
